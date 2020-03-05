7e455887a642eddf7a020d89b07ca1b700af5fc7
Kevin Xu <yadadooda@gmail.com>
2020-03-04 19:17:03 -0500

    Week 4 Movie List

---
+++ Week4CodeAlong/Week4CodeAlong/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Week4CodeAlong
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Movie(string myTitle, int myReleaseYear)
+        {
+            this.Title = myTitle;
+            this.ReleaseYear = myReleaseYear;
+        }
+
+        public void ShowDetails()
+        {
+            string info = "Title: " + this.Title;
+            info += "\nRelease Year: " + this.ReleaseYear;
+            MessageBox.Show(info);
+        }
+    }
+}
